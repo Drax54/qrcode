7cf1efbadeab6c173fc8976e69a8bd976e9aa1a5
Drax54 <dhcp73@gmail.com>
2024-08-28 20:38:08 +0100

    Specify Python version to 3.10

--- app.py
+++ app.py
@@ -18,7 +18,12 @@ app = Flask(__name__)
 # Update the SQLALCHEMY_DATABASE_URI to use PostgreSQL
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgresql://postgres:root@localhost/myproject')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+
+database_url = os.getenv('DATABASE_URL')
+if database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
